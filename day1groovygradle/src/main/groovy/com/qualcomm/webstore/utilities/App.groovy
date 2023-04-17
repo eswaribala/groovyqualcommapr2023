66c58c8ed900cf6590fd8a73bdfcad75e2886fcf
eswaribala <parameswaribala@gmail.com>
2023-04-17 14:45:48 +0530

    updated

--- day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/App.groovy
+++ day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/App.groovy
@@ -32,3 +32,10 @@ All applications which rely on serialization and do not isolate the code which d
 are subject to this vulnerability."""
 
 println "${description}"
+pattern=/
+[a-zA-Z]*/
+
+println "${pattern}"
+
+def productId=new Random().nextInt(100).plus(1000)
+println "ProductId = ${productId}"

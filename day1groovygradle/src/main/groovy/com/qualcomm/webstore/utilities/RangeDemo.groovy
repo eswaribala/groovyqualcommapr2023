71344a5e62640c1e11567ec2e14682eeb2f54de5
eswaribala <parameswaribala@gmail.com>
2023-04-17 15:20:42 +0530

    updated

---
+++ day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/RangeDemo.groovy
@@ -0,0 +1,6 @@
+package com.qualcomm.webstore.utilities
+
+//range of values
+for (data in 100 .. 200){
+    println "Data ${data} having random value as ${new Random().nextInt(10000)}"
+}
--- day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/RegExpDemo.groovy
+++ day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/RegExpDemo.groovy
@@ -16,3 +16,11 @@ def info="Product having Product Id 1234 found in records"
 
 assert info =~ pattern
 
+def namePattern= ~/[a-zA-Z]{5,25}/
+def firstName="Parameswari"
+assert firstName =~ namePattern
+
+//matcher operator
+def matches = namePattern.matcher(firstName)
+
+assert matches.matches()

cf8051d97332d32b56fde0e0130b3ca3be5413b0
eswaribala <parameswaribala@gmail.com>
2023-04-17 14:36:12 +0530

    updated

--- day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/App.groovy
+++ day1groovygradle/src/main/groovy/com/qualcomm/webstore/utilities/App.groovy
@@ -1,5 +1,5 @@
 package com.qualcomm.webstore.utilities
-
+/*
 println("Rocking with gradle!!!")
 //method 1
 println("Enter Product Id")
@@ -17,7 +17,18 @@ println "Expiry Date ${expiryDate}"
 
 //method 3
 def scanner =new Scanner(System.in)
-
+println("Enter the cost")
 def cost=scanner.nextInt()
 scanner.nextLine()
 println "Cost= ${cost}"
+
+println cost.class.name
+*/
+def description="""When an application has Groovy on the classpath 
+and uses standard Java serialization mechanisms to communicate between servers, 
+or to store local data, it is possible for an attacker to bake a special serialized object 
+that will execute code directly when deserialized. 
+All applications which rely on serialization and do not isolate the code which deserializes objects 
+are subject to this vulnerability."""
+
+println "${description}"

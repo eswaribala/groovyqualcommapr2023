4369d132845cbb1ead059651b5fe5ed7c45af14b
eswaribala <parameswaribala@gmail.com>
2023-04-18 16:23:48 +0530

    updated

---
+++ day2sbwebstore/src/main/groovy/com/qualcomm/day2sbwebstore/Day2sbwebstoreApplication.groovy
@@ -0,0 +1,13 @@
+package com.qualcomm.day2sbwebstore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Day2sbwebstoreApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(Day2sbwebstoreApplication, args)
+    }
+
+}
---
+++ day2sbwebstore/src/test/groovy/com/qualcomm/day2sbwebstore/Day2sbwebstoreApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.qualcomm.day2sbwebstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Day2sbwebstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

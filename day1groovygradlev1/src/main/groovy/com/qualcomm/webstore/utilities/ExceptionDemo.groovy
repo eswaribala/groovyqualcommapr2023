f3e6398410468c5255c6515f475c2099bbc9c082
eswaribala <parameswaribala@gmail.com>
2023-04-18 10:36:36 +0530

    updated

--- day1groovygradlev1/src/main/groovy/com/qualcomm/webstore/utilities/ExceptionDemo.groovy
+++ day1groovygradlev1/src/main/groovy/com/qualcomm/webstore/utilities/ExceptionDemo.groovy
@@ -13,7 +13,7 @@ class ExceptionDemo {
         try {
             def userAccount = new UserAccount(new Random().nextInt(1000000), "Test@123",
                     LocalDate.of(1980 + new Random().nextInt(20), 1 + new Random().nextInt(10), 1 + new Random().nextInt(26)),
-                    "", "", [new Address(new Random().nextInt(10000), "8d", "Chennai", "TN", 600049)])
+                    "Parameswari", "", [new Address(new Random().nextInt(10000), "8d", "Chennai", "TN", 600049)])
         }
         catch (NameException ex){
             println(ex.message)
---
+++ day1groovygradlev1/src/main/groovy/com/qualcomm/webstore/utilities/IteratorsDemo.groovy
@@ -0,0 +1,49 @@
+package com.qualcomm.webstore.utilities
+
+import com.qualcomm.webstore.models.Address
+import com.qualcomm.webstore.models.Order
+import com.qualcomm.webstore.models.UserAccount
+
+import java.time.LocalDate
+
+def ordersMap=[:]
+
+
+for(count in 1 .. 5){
+    ordersMap.put(count,new Order(new UserAccount(new Random().nextInt(1000000),"Test@123",
+            LocalDate.of(1980+new  Random().nextInt(20), 1+new Random().nextInt(10), 1+ new Random().nextInt(26)),
+            "Parameswari","Balasubramaniam",[new Address(new Random().nextInt(10000),"8d","Chennai","TN",600049)]),
+            LocalDate.of(1980+new  Random().nextInt(20), 1+new Random().nextInt(10), 1+ new Random().nextInt(26))))
+}
+
+ordersMap.each {
+    println(it.key)
+    println(((Order)it.value).getDor())
+    println(((Order)it.value).getUserAccount().getUserId())
+}
+
+ordersMap.each { entry -> println "Key: $entry.key, Value: $entry.value" }
+
+ordersMap.each { key, val ->
+    println "Key: $key , Value: $val"
+}
+
+ordersMap.eachWithIndex { entry, index ->
+   // def indent = ((index == 0 || index % 2 == 0) ? "   " : "")
+    println "Index: $index, Key: $entry.key, Value: $entry.value"
+}
+
+
+
+
+
+List<UserAccount> userAccounts=new ArrayList<UserAccount>()
+//range of users
+for(count in 1 .. 5){
+    userAccounts.add(new UserAccount(new Random().nextInt(1000000),"Test@123",
+            LocalDate.of(1980+new  Random().nextInt(20), 1+new Random().nextInt(10), 1+ new Random().nextInt(26)),
+            "Parameswari","Balasubramaniam",[new Address(new Random().nextInt(10000),"8d","Chennai","TN",600049)]))
+}
+
+userAccounts.each {println "${it}"}
+userAccounts.eachWithIndex {it,index->println "${index},${it}"}

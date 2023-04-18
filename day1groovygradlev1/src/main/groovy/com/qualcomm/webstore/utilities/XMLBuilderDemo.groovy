c7bcd3cce2557f860d786d092364e533a310ef64
eswaribala <parameswaribala@gmail.com>
2023-04-18 12:37:24 +0530

    updated

--- day1groovygradlev1/src/main/groovy/com/qualcomm/webstore/utilities/XMLBuilderDemo.groovy
+++ day1groovygradlev1/src/main/groovy/com/qualcomm/webstore/utilities/XMLBuilderDemo.groovy
@@ -4,17 +4,21 @@ package com.qualcomm.webstore.utilities
 
 def builder = new groovy.xml.MarkupBuilder()
 //builder. Root element name
-builder.Projects {
-    description 'Project and Employees of 10..15'
-    for (project in 10..15) {
+builder.Employees {
+    //description 'Project and Employees of 10..15'
+    for (employee in 10..15) {
         //Project Sub Element Name
-        Project (project: project, employee: new Random().nextInt(10000)) {
-            for (j in 2..<project) {
-                if (project % j == 0) {
+        Employee (employeeId: employee, projectId: new Random().nextInt(10000)) {
+            for (j in 2..<employee) {
+                if (employee % j == 0) {
                     //Team is sub element of Project
-                    Team (value: j)
+                    Benefit (value: j){
+                        Welfare(value:j)
+                    }
                 }
             }
         }
     }
 }
+//check
+//new File('out.xml').write(builder as String)

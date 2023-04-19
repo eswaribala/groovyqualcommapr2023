c726546031e28df7699f5a7698a2f5b330067486
eswaribala <parameswaribala@gmail.com>
2023-04-19 15:44:09 +0530

    updated

---
+++ day3/grails-app/services/day3/AwardService.groovy
@@ -0,0 +1,18 @@
+package day3
+
+import grails.gorm.services.Service
+
+@Service(Award)
+interface AwardService {
+
+    Award get(Serializable id)
+
+    List<Award> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Award save(Award award)
+
+}
---
+++ day3/grails-app/services/day3/OnlineOrderService.groovy
@@ -0,0 +1,18 @@
+package day3
+
+import grails.gorm.services.Service
+
+@Service(OnlineOrder)
+interface OnlineOrderService {
+
+    OnlineOrder get(Serializable id)
+
+    List<OnlineOrder> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    OnlineOrder save(OnlineOrder onlineOrder)
+
+}

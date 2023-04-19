b09c26f78bd6a299cae381d7675329a4e4aa2cec
eswaribala <parameswaribala@gmail.com>
2023-04-19 15:03:41 +0530

    updated

--- day3/grails-app/domain/day3/Product.groovy
+++ day3/grails-app/domain/day3/Product.groovy
@@ -1,12 +1,27 @@
 package day3
 
+import grails.rest.Resource
+
+@Resource(uri='/api/products', formats=['json', 'xml'])
 class Product {
 
     Long productId
     String productName
     Integer cost
-    static hasMany = [orderItems:OrderItem]
 
+    Product(version, id, Long productId, String productName, Integer cost) {
+        this.version = version
+        this.id = id
+        this.productId = productId
+        this.productName = productName
+        this.cost = cost
+    }
+    static hasMany = [orderItems:OrderItem]
+    static mapping = {
+        version false
+      //  table 'product'
+       // id column: 'productId', generator:'native', params:[sequence:'order_seq']
+    }
     static constraints = {
         productId()
         productName()
--- day3/grails-app/init/day3/BootStrap.groovy
+++ day3/grails-app/init/day3/BootStrap.groovy
@@ -3,6 +3,8 @@ package day3
 class BootStrap {
 
     def init = { servletContext ->
+        new Product(0,1,1L,'Pencil',50).save()
+
     }
     def destroy = {
     }

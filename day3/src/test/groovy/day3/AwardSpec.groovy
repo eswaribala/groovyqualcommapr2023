3efb2629529e2155973d696979616df9e03e0adf
eswaribala <parameswaribala@gmail.com>
2023-04-20 17:02:15 +0530

    updated

--- day3/src/test/groovy/day3/AwardSpec.groovy
+++ day3/src/test/groovy/day3/AwardSpec.groovy
@@ -3,7 +3,7 @@ package day3
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
-class AwardSpec extends Specification implements DomainUnitTest<Award> {
+class AwardSpec extends Specification {
 
     def setup() {
     }
@@ -11,6 +11,23 @@ class AwardSpec extends Specification implements DomainUnitTest<Award> {
     def cleanup() {
     }
 
+    def "#award point test"() {
+        expect:
+        award.getPoints() == points
+
+        where:
+        award                   || points
+        new Award(type: "Excellent")  || "5"
+        new Award(type: "VeryGood") || "4"
+    }
+
+    static class Award {
+        Integer points
+        String type
+        String getPoints() {
+            type == "Excellent" ? 5 : 3
+        }
+    }
     void "test something"() {
         expect:"fix me"
             true == false

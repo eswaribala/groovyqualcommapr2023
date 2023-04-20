674c2f1aeb39e6ba0806ae3d5b79b05c01b080e3
eswaribala <parameswaribala@gmail.com>
2023-04-20 15:09:30 +0530

    updated

--- day3/src/test/groovy/day3/TestProduct.groovy
+++ day3/src/test/groovy/day3/TestProduct.groovy
@@ -2,11 +2,24 @@ package day3
 
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+
+import java.time.LocalDate
+import java.util.stream.Stream
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertFalse
 import static org.junit.jupiter.api.Assertions.assertNotNull
 import static org.junit.jupiter.api.Assertions.assertTrue
+import static org.junit.jupiter.params.provider.Arguments.arguments
+import static org.junit.jupiter.params.provider.Arguments.arguments
+import static org.junit.jupiter.params.provider.Arguments.arguments
+import static org.junit.jupiter.params.provider.Arguments.arguments
+
 //import static org.junit.jupiter.params.provider.Arguments.arguments
 
 class TestProduct {
@@ -21,5 +34,35 @@ class TestProduct {
 
         assertNotNull(product)
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [8434659, 42858, 244,0,23576])
+    void productCostAboveZeroTest(int cost) {
+
+        product.setCost(cost)
+        assertTrue(product.getCost()>0)
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("generateProductData")
+    void productNameLengthTest(int productId,String productName, int cost) {
+        product.setProductId(productId)
+        product.setProductName(productName)
+        product.setCost(cost)
+        assertTrue(product.getProductName().length()>5)
+
+    }
+
+    static Stream<Arguments> generateProductData() {
+
+        return Stream.of(
+                arguments(1,"Mouse",400),
+                arguments(2,"KeyBoard",660),
+                arguments(3,"Pouch",567)
+
+
+        );
     }
 }
